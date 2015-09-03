051c7b67201ae53436a9069e23370c991ab2963a
flan <flan@flande.re>
2015-09-03 19:52:30 +0200

    add more options for id parsing

--- vnid.py
+++ vnid.py
@@ -15,6 +15,14 @@ CREDENTIALS = {
 API_IP = '188.165.233.33'
 API_PORT = 19534
 
+class Item:
+  def __init__(self, original_arg, type_="", id_=0):
+    self.original_arg = original_arg
+    self.type = type_
+    self.id = id_
+  def __str__(self):
+    return "{\"%s\", \"%s\", %i}" % (self.original_arg, self.type, self.id)
+
 if len(sys.argv) < 2:
   print("usage: %s [id]..." % os.path.basename(sys.argv[0]))
   exit(2)
@@ -34,8 +42,25 @@ def cmd(s, msg):
 
   return data
 
+# id ::= "v" <vn-id> | "r" <release-id> | <vn-id> "_" <release-id> | <vn-id>
+def parse_id(id_):
+  if id_.startswith("v"): # "v" <vn-id>
+    return Item(id_, "vn", int(id_[1:]))
+  elif id_.startswith("r"): # "r" <release-id>
+    return Item(id_, "release", int(id_[1:]))
+  elif '_' in id_: # <vn-id> "_" <release-id>
+    split_id = id_.split('_')
 
-ids = json.dumps(sys.argv[1:])
+    if len(split_id) != 2:
+      raise ValueError("invalid id")
+
+    # XXX: (maybe) check if release id matches vn id (would introduce side effects)
+    return Item(id_, "release", int(split_id[1]))
+  else: # <vn-id>
+    return Item(id_, "vn", int(id_))
+
+ids = [parse_id(x).id for x in sys.argv[1:] if parse_id(x).type == "vn"]
+ids = json.dumps(ids)
 s.connect((API_IP, API_PORT))
 
 cmd(s, "login " + json.dumps(CREDENTIALS))
