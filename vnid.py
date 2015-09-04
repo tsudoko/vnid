379b7de4a88bba29541c9d751d9987ccdb8a859e
flan <flan@flande.re>
2015-09-04 06:40:56 +0200

    partially conform to pep8
    Fixes E111, W293, E302, and E701

--- vnid.py
+++ vnid.py
@@ -5,9 +5,9 @@ import sys
 import os
 
 CREDENTIALS = {
-  "protocol": 1,
-  "client": "vnid",
-  "clientver": "0.3.0",
+    "protocol": 1,
+    "client": "vnid",
+    "clientver": "0.3.2",
 }
 
 #API_IP = '188.165.210.64'
@@ -15,95 +15,105 @@ CREDENTIALS = {
 API_IP = '188.165.233.33' # api.vndb.org takes a while to resolve for some reason, so we're using the IP address
 API_PORT = 19534
 
+
 class VNDBError(Exception):
-  pass
+    pass
+
 
 class Item:
-  def __init__(self, original_arg, type_="", id_=0, title=""):
-    self.original_arg = original_arg
-    self.type = type_
-    self.id = id_
-    self.title = title
-  def __str__(self):
-    return "{\"%s\" \"%s\" %i \"%s\"}" % (self.original_arg, self.type, self.id, self.title)
+    def __init__(self, original_arg, type_="", id_=0, title=""):
+        self.original_arg = original_arg
+        self.type = type_
+        self.id = id_
+        self.title = title
+
+    def __str__(self):
+        return "{\"%s\" \"%s\" %i \"%s\"}" % (self.original_arg, self.type, self.id, self.title)
 
 if len(sys.argv) < 2:
-  print("usage: %s [id]..." % os.path.basename(sys.argv[0]))
-  exit(2)
+    print("usage: %s [id]..." % os.path.basename(sys.argv[0]))
+    exit(2)
 
 s = socket.socket()
 
+
 def parse_response(response):
-  r = response.split(' ')
+    r = response.split(' ')
 
-  name = r[0]
-  json_ = json.loads(' '.join(r[1:]))
-  print(json_)
+    name = r[0]
+    json_ = json.loads(' '.join(r[1:]))
+    print(json_)
 
-  if name == "error":
-    raise VNDBError(json_['id'] + (": " + json_['msg'] if "msg" in json_ and json_['msg'] else ''))
+    if name == "error":
+        raise VNDBError(json_['id'] + (": " + json_['msg'] if "msg" in json_ and json_['msg'] else ''))
 
-  return json_
+    return json_
+
 
 def cmd(s, msg):
-  data = ""
-  now = b""
+    data = ""
+    now = b""
 
-  s.send(bytes(msg + "\x04", "UTF-8"))
+    s.send(bytes(msg + "\x04", "UTF-8"))
 
-  while not now.endswith(b"\x04"):
-    now = s.recv(1024)
-    if not now: break # should protect from infinite loop on a broken connection
-    data += now.decode().strip("\x04")
+    while not now.endswith(b"\x04"):
+        now = s.recv(1024)
+        if not now:
+            break # should protect from infinite loop on a broken connection
+        data += now.decode().strip("\x04")
 
-  return data
+    return data
+
 
 # id ::= "v" <vn-id> | "r" <release-id> | <vn-id>
 def parse_id(id_):
-  if id_.startswith("v"): # "v" <vn-id>
-    return Item(id_, "vn", int(id_[1:]))
-  elif id_.startswith("r"): # "r" <release-id>
-    return Item(id_, "release", int(id_[1:]))
-  else: # <vn-id>
-    return Item(id_, "vn", int(id_))
+    if id_.startswith("v"): # "v" <vn-id>
+        return Item(id_, "vn", int(id_[1:]))
+    elif id_.startswith("r"): # "r" <release-id>
+        return Item(id_, "release", int(id_[1:]))
+    else: # <vn-id>
+        return Item(id_, "vn", int(id_))
+
 
 def cmd_query_items(s, items, flags="basic"):
-  queries = {} # key: exact query command (no filters), value: id list
-  results = []
+    queries = {} # key: exact query command (no filters), value: id list
+    results = []
 
-  for i in items:
-    if type(i) != Item:
-      raise TypeError(str(i) + ": the \'items\' argument of query_items() can only take lists of the Item class instances (got " + type(i) + ")")
+    for i in items:
+        if type(i) != Item:
+            raise TypeError(str(i) + ": the \'items\' argument of query_items() can only take lists of the Item class instances (got " + type(i) + ")")
 
-    query_cmd = "get " + i.type + " " + flags
+        query_cmd = "get " + i.type + " " + flags
 
-    if query_cmd in queries and queries[query_cmd]:
-      queries[query_cmd].append(i.id)
-    else:
-      queries[query_cmd] = [i.id]
-
-  #print(queries)
-
-  for k, v in queries.items():
-    #print(parse_response(cmd(s, k + " (id = " + json.dumps(v) + ")")))['items']
-    i = 1
-
-    while True:
-      response = parse_response(cmd(s, k + " (id = " + json.dumps(v) + ") " + json.dumps({"page": i})))
-      results += response['items']
-      if not response['more']: break
-      i += 1
-      
-
-  for i in items:
-    for j in results:
-      if i.id == j['id']:
-        if j['original']:
-          i.title = j['original']
+        if query_cmd in queries and queries[query_cmd]:
+            queries[query_cmd].append(i.id)
         else:
-          i.title = j['title']
+            queries[query_cmd] = [i.id]
 
-  return items
+    #print(queries)
+
+    for k, v in queries.items():
+        #print(parse_response(cmd(s, k + " (id = " + json.dumps(v) + ")")))['items']
+        i = 1
+
+        while True:
+            response = parse_response(cmd(s, k + " (id = " + json.dumps(v) + ") " + json.dumps({"page": i})))
+            results += response['items']
+
+            if not response['more']:
+                break
+
+            i += 1
+
+    for i in items:
+        for j in results:
+            if i.id == j['id']:
+                if j['original']:
+                    i.title = j['original']
+                else:
+                    i.title = j['title']
+
+    return items
 
 
 items = [parse_id(x) for x in sys.argv[1:]]
@@ -113,6 +123,6 @@ cmd(s, "login " + json.dumps(CREDENTIALS))
 items = cmd_query_items(s, items)
 
 for i in items:
-  print(i.original_arg + '\t' + i.title)
+    print(i.original_arg + '\t' + i.title)
 
 s.close()
